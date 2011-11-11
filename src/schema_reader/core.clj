ceeeb8cc82d7f4a055130d914e1c4151481f600e
Ron Toland <rontoland@gmail.com>
2011-11-11 11:31:07 -0500

    Modify sql-query function to wrap up the common with-connection tasks. Add function to pull out a sequence of maps describing the tables and columns in the given database.

--- src/schema_reader/core.clj
+++ src/schema_reader/core.clj
@@ -1,24 +1,39 @@
 (ns schema-reader.core
-  (:require [clojure.java.jdbc :as sql]))
+  (:require [clojure.java.jdbc :as sql])
+)
 
-; define the database connection
-(def db {:subprotocol "postgresql" :subname "//localhost:5432/schema_test" :username "rontoland" :password ""})
+;; define the database connection
+(def db 
+  {:subprotocol "postgresql" 
+   :subname "//localhost:5432/schema_test"
+   :username "schema_reader"
+   :password ""
+  }
+)
 
 (defn sql-query [q]
-  (sql/with-query-results res q (doall res)
+  (seq (sql/with-connection db (sql/with-query-results res q (doall res))))
+)
+
+(defn get-table-list []
+  (map :tablename (sql-query
+    [(str "SELECT tablename FROM pg_tables WHERE tablename NOT LIKE 'pg%' AND tablename NOT LIKE 'sql%';")])
   )
 )
 
 (defn get-table-schema [table]
-  (seq
-    (sql/with-connection db
-      (sql-query
-        [(str "SELECT attname
-               FROM pg_attribute, pg_type
-               WHERE typname = '" table "' AND attrelid = typrelid
-               AND attname NOT IN ('cmin', 'cmax', 'xmin', 'xmax', 'ctid', 'tableoid');"
-        )]
-      )
-    )
+  (sql-query
+    [(str "SELECT attname
+           FROM pg_attribute, pg_type
+           WHERE typname = '" table "' AND attrelid = typrelid
+           AND attname NOT IN ('cmin', 'cmax', 'xmin', 'xmax', 'ctid', 'tableoid');"
+    )]
+  )
+)
+
+;; get a sequence of maps describing the db tables
+(defn get-db-schema []
+  (for [table (get-table-list)]
+    {:tablename table :columns (vec (map :attname (get-table-schema table)))}
   )
 )
